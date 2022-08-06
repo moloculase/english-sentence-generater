36e77e445e47c3492338284353072c198988127e
moloculase <87440666+moloculase@users.noreply.github.com>
2022-08-06 09:32:49 +0500

    Delete error.php

--- error.php
+++
@@ -1,9 +0,0 @@
-<!-- 
-mandarin bot\
-by: ahmed adhiyyu mohamed hassan
-email: adhiyyuahmed@gmail.com
--->
-<h1>ERROR</h1>
-<img src="warning.png">
-
-<p style="text-align: left; color: red;"><?=$_GET['error'] ?? "error"?></p>
